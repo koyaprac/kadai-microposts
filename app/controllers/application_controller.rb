a8486d92e2e5c79db715b552c7fe5ee863b2b94c
koya <sereronnfox@gmail.com>
2018-04-28 09:14:27 +0000

    post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  private
+  
+  include SessionsHelper
+  
+  def require_user_logged_in
+    unless logged_in?
+      redirect_to login_url
+    end
+  end
+  
+  def counts(user)
+    @count_microposts = user.microposts.count
+  end
 end
