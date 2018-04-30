fa3d1dd3f69c7acc2c683f5e170907d75567b684
koya <sereronnfox@gmail.com>
2018-04-30 10:46:04 +0000

    bag fix

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,13 +5,13 @@ class LikesController < ApplicationController
     micropost = Micropost.find(params[:micropost_id])
     current_user.favorite(micropost)
     flash[:success] = 'You successfully favorited it'
-    redirect_to root_url
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     micropost = Micropost.find(params[:micropost_id])
     current_user.unfavorite(micropost)
     flash[:success] = 'You successfully unfavorited it'
-    redirect_to root_url
+    redirect_back(fallback_location: root_path)
   end
 end
