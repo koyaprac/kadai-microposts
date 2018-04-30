4f57969312ae198813e23fea2f83589f677bab27
koya <sereronnfox@gmail.com>
2018-04-30 10:12:55 +0000

    likes

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,5 +4,5 @@ class Micropost < ApplicationRecord
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 255}
   
-  
+  has_many :favorited, through: :like, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,23 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
+
+  # Likes
+  has_many :likes
+  has_many :favorites, through: :likes, source: :micropost
+  
+  def favorite(micropost)
+    self.likes.find_or_create_by(micropost_id: micropost.id)
+  end
+  
+  def unfavorite(micropost)
+    favorite = self.likes.find_by(micropost_id: micropost.id)
+    favorite.destroy if favorite
+  end
+
+  def favorite?(micropost)
+    self.favorites.include?(micropost)
+  end
   
   def follow(other_user)
     unless self == other_user
@@ -31,4 +48,6 @@ class User < ApplicationRecord
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
   end
+  
+  
 end
